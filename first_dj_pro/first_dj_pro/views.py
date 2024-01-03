29b1ca3d7c17a6ad7647b2232d8686a276b210b1
saradha <saradhashyam0401@gmail.com>
2024-01-03 13:31:30 +0530

    just

--- first_dj_pro/first_dj_pro/views.py
+++ first_dj_pro/first_dj_pro/views.py
@@ -2,7 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def homePage(request):
-    return render(request,"index.html")
+    data = {    
+        'title':"HOME PAGE",
+        'meow':"meow meow",
+        'loop': ["hai","hello","meow"],
+        'di_list':[
+                {'name':"meow", 'num':"876555454"},
+                {'name':"bow", 'num':"65156456554"}
+            ]
+    }
+    return render(request,"index.html",data)
 
 def aboutUs(request):
     return HttpResponse("Hi I'm Saradha.. This is my first django project")
