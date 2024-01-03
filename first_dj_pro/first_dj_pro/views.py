2df34f1e3a126a357f082db5702c7e7d9d88f4cf
saradha <saradhashyam0401@gmail.com>
2024-01-03 09:58:27 +0530

    course detail and course added for dynamic

--- first_dj_pro/first_dj_pro/views.py
+++ first_dj_pro/first_dj_pro/views.py
@@ -2,3 +2,9 @@ from django.http import HttpResponse
 
 def aboutUs(request):
     return HttpResponse("Hi I'm Saradha.. This is my first django project")
+
+def course(request):
+    return HttpResponse("This is the course page")
+
+def courseDetails(request,courseid):
+    return HttpResponse(courseid)
